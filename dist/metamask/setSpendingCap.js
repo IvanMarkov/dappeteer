3243ed0d7b9e1ab85e42e7066ad4b2c386a6ae3a
Ivan Markov <vanymarkov1996@gmail.com>
2023-07-26 14:37:28 +0200

    Fix confirmTransaction for all chains

--- dist/metamask/setSpendingCap.js
+++ dist/metamask/setSpendingCap.js
@@ -8,9 +8,13 @@ const setSpendingCap = (page) => async () => {
   await page.bringToFront();
   await page.reload();
   await (0, helpers_1.waitForOverlay)(page);
-  await (0, helpers_1.clickOnButton)(page, "Use default");
-  await page.waitForTimeout(1000);
-  await (0, helpers_1.clickOnButton)(page, "Next");
-  await (0, helpers_1.clickOnButton)(page, "Approve");
+  try {
+    await (0, helpers_1.clickOnButton)(page, "Confirm", { timeout: 500 });
+  } catch (e) {
+    await (0, helpers_1.clickOnButton)(page, "Use default");
+    await page.waitForTimeout(1000);
+    await (0, helpers_1.clickOnButton)(page, "Next");
+    await (0, helpers_1.clickOnButton)(page, "Approve");
+  }
 };
 exports.setSpendingCap = setSpendingCap;
